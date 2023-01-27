8ed01fb502e40d9535b98d63dacd74c6e05eba71
Colby Shabel <jcshabel@gmail.com>
2023-01-26 18:09:33 -0600

    help

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+
+
+
+const newBee =[]
+function writeCards(on,sup){
+   
+    for(let i = 0; i < on.length; i++){
+       newBee.push(`Thank you, ${on[i]}, for the wonderful ${sup} gift!`)
+     
+}
+return newBee
+}
+writeCards(["Guadalupe", "Ollie", "Aki"],"surprise")
+
+
+// function countDown(numb){
+
+//     // for(let i = 0; i <= numb; i--){
+        
+//     // }
+//      while(numb >= 0){
+        
+//          console.log(numb)
+//          numb--
+        
+//      }
+//  }
+//  countDown(10)
